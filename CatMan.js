3cd57ece75c6e87469e595773fc5d7abf7bb5dff
Aasim Syed <65820963+Awesome-Aasim@users.noreply.github.com>
2021-02-08 17:52:59 -0800

    Add licensing information and information about contributing

--- CatMan.js
+++ CatMan.js
@@ -1,3 +1,35 @@
+/***********************************************************************************
+ * CatMan:  The category manager for MediaWiki wikis that is simple, lightweight,  *
+ * and modern.                                                                     *
+ *                                                                                 *
+ * This script is largely a work in progress and may sometimes not work properly.  *
+ * Please see the English Wikipedia for known problems that are currently being    *
+ * fixed.                                                                          *
+ ***********************************************************************************
+ * IMPORTANT (particularly for those who are viewing a copy of the script):        *
+ * THIS FILE IS AVAILABLE ON GITHUB AT https://github.com/Awesome-Aasim/CatMan     *
+ * ALL PULL REQUESTS AND CHANGES MUST BE MADE THERE, OTHERWISE THEY WILL BE LOST   *
+ * IF THE FILE AT THE REPOSITORY IS CHANGED OR REBUILT.                            *
+ ***********************************************************************************
+ * LICENSING INFORMATION: Licensed under the MIT license                           *
+ * MIT License                                                                     *
+ * Copyright (c) 2021 Aasim Syed                                                   *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy    *
+ * of this software and associated documentation files (the "Software"), to deal   *
+ * in the Software without restriction, including without limitation the rights    *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell       *
+ * copies of the Software, and to permit persons to whom the Software is           *
+ * furnished to do so, subject to the following conditions:                        *
+ * The above copyright notice and this permission notice shall be included in all  *
+ * copies or substantial portions of the Software.                                 *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   *
+ * SOFTWARE.                                                                       *
+ ***********************************************************************************/
 if (!Catman && mw.config.get("wgNamespaceNumber") >= 0 && mw.config.get("wgIsProbablyEditable")) {
     var Catman = {};
     mw.loader.using(["oojs-ui-core", "oojs-ui-windows", "oojs-ui-widgets", "oojs-ui.styles.icons-moderation", "oojs-ui.styles.icons-interactions", "oojs-ui.styles.icons-editing-core"], function () {
